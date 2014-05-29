a158f0a55099417fb574e09e9cbf4c63d5c9bc18
Thomas S Hatch <thatch45@gmail.com>
2014-05-29 15:20:52 -0600

    make python3 compat

--- nacl/__init__.py
+++ nacl/__init__.py
@@ -12,32 +12,32 @@ if sys.platform.startswith('win'):
 else:
     libnacl = ctypes.cdll.LoadLibrary('libsodium.so')
 
-crypto_box_PUBLICKEYBYTES = 32L
-crypto_box_SECRETKEYBYTES = 32L
-crypto_box_BEFORENMBYTES = 32L
-crypto_box_NONCEBYTES = 24L
-crypto_box_ZEROBYTES = 32L
-crypto_box_BOXZEROBYTES = 16L
-crypto_sign_BYTES = 64L
-crypto_sign_PUBLICKEYBYTES = 32L
-crypto_sign_SECRETKEYBYTES = 64L
-crypto_sign_SEEDBYTES = 32L
+crypto_box_PUBLICKEYBYTES = 32
+crypto_box_SECRETKEYBYTES = 32
+crypto_box_BEFORENMBYTES = 32
+crypto_box_NONCEBYTES = 24
+crypto_box_ZEROBYTES = 32
+crypto_box_BOXZEROBYTES = 16
+crypto_sign_BYTES = 64
+crypto_sign_PUBLICKEYBYTES = 32
+crypto_sign_SECRETKEYBYTES = 64
+crypto_sign_SEEDBYTES = 32
 crypto_box_MACBYTES = crypto_box_ZEROBYTES - crypto_box_BOXZEROBYTES
-crypto_secretbox_KEYBYTES = 32L
-crypto_secretbox_NONCEBYTES = 24L
-crypto_secretbox_KEYBYTES = 32L
-crypto_secretbox_ZEROBYTES = 32L
-crypto_secretbox_BOXZEROBYTES = 16L
+crypto_secretbox_KEYBYTES = 32
+crypto_secretbox_NONCEBYTES = 24
+crypto_secretbox_KEYBYTES = 32
+crypto_secretbox_ZEROBYTES = 32
+crypto_secretbox_BOXZEROBYTES = 16
 crypto_secretbox_MACBYTES = crypto_secretbox_ZEROBYTES - crypto_secretbox_BOXZEROBYTES
-crypto_stream_KEYBYTES = 32L
-crypto_stream_NONCEBYTES = 24L
-crypto_auth_BYTES = 32L
-crypto_auth_KEYBYTES = 32L
-crypto_onetimeauth_BYTES = 16L
-crypto_onetimeauth_KEYBYTES = 32L
-crypto_generichash_BYTES = 32L
-crypto_scalarmult_curve25519_BYTES = 32L
-crypto_scalarmult_BYTES = 32L
+crypto_stream_KEYBYTES = 32
+crypto_stream_NONCEBYTES = 24
+crypto_auth_BYTES = 32
+crypto_auth_KEYBYTES = 32
+crypto_onetimeauth_BYTES = 16
+crypto_onetimeauth_KEYBYTES = 32
+crypto_generichash_BYTES = 32
+crypto_scalarmult_curve25519_BYTES = 32
+crypto_scalarmult_BYTES = 32
 crypto_hash_BYTES = 64
 
 # Pubkey defs
