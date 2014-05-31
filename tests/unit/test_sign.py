f03f976696077db4146ea78e0d0b1ef5767f00ca
Thomas S Hatch <thatch45@gmail.com>
2014-05-31 11:14:12 -0600

    Add high level signing capabilities

---
+++ tests/unit/test_sign.py
@@ -0,0 +1,19 @@
+# Import libnacl libs
+import libnacl.sign
+
+# Import pythonlibs
+import unittest
+
+
+class TestSigning(unittest.TestCase):
+    '''
+    '''
+    def test_sign(self):
+        msg = ('Well, that\'s no ordinary rabbit.  That\'s the most foul, '
+               'cruel, and bad-tempered rodent you ever set eyes on.')
+        signer = libnacl.sign.Signer()
+        signed = signer.sign(msg)
+        self.assertNotEqual(msg, signed)
+        veri = libnacl.sign.Verifier(signer.hex_vk())
+        verified = veri.verify(signed)
+        self.assertEqual(verified, msg)
