f5f9044e8498382729879a7702d76631d0d75f96
Thomas S Hatch <thatch45@gmail.com>
2014-06-24 15:25:23 -0600

    Add libnacl.utils.load_key

--- libnacl/utils.py
+++ libnacl/utils.py
@@ -61,6 +61,37 @@ class BaseKey(object):
         os.umask(cumask)
 
 
+def load_key(path, serial='json'):
+    '''
+    Read in a key from a file and return the applicable key object based on
+    the contents of the file
+    '''
+    with open(path, 'rb') as fp_:
+        packaged = fp_.read()
+    if serial == 'msgpack':
+        import msgpack
+        key_data = msgpack.loads(packaged)
+    elif serial == 'json':
+        import json
+        key_data = json.loads(packaged)
+    if 'priv' and 'seed' in key_data:
+        return libnacl.dual.DualSecret(
+                libnacl.encode.hex_decode(key_data['priv']),
+                libnacl.encode.hex_decode(key_data['seed']))
+    elif 'priv' in key_data:
+        return libnacl.public.SecretKey(
+                libnacl.encode.hex_decode(key_data['priv']))
+    elif 'sign' in key_data:
+        return libnacl.sign.Signer(
+                libnacl.encode.hex_decode(key_data['sign']))
+    elif 'pub' in key_data:
+        return libnacl.public.PublicKey(
+                libnacl.encode.hex_decode(key_data['pub']))
+    elif 'verify' in key_data:
+        return libnacl.sign.Verifier(key_data['verify'])
+    raise ValueError('Found no key data')
+
+
 def salsa_key():
     '''
     Generates a salsa2020 key
