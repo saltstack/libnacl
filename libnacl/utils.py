21025cb75b10b9ef5d064f24757f36cf8c7094e0
Thomas S Hatch <thatch45@gmail.com>
2014-06-24 13:42:00 -0600

    Add save method to basekey

--- libnacl/utils.py
+++ libnacl/utils.py
@@ -5,6 +5,7 @@ import libnacl
 import libnacl.encode
 
 # Import python libs
+import os
 import time
 import binascii
 
@@ -30,6 +31,34 @@ class BaseKey(object):
     def hex_seed(self):
         if hasattr(self, 'seed'):
             return libnacl.encode.hex_encode(self.seed)
+
+    def save(self, path, serial='json'):
+        '''
+        Safely save keys with perms of 0400
+        '''
+        pre = {}
+        sk = self.hex_sk()
+        pk = self.hex_pk()
+        vk = self.hex_vk()
+        seed = self.hex_seed()
+        if sk:
+            pre['priv'] = sk
+        if pk:
+            pre['pub'] = pk
+        if vk:
+            pre['verify'] = vk
+        if seed:
+            pre['sign'] = seed
+        if serial == 'msgpack':
+            import msgpack
+            packaged = msgpack.dumps(pre)
+        elif serial == 'json':
+            import json
+            packaged = json.dumps(pre)
+        cumask = os.umask(191)
+        with open(path, 'wb+') as fp_:
+            fp_.write(packaged)
+        os.umask(cumask)
 
 
 def salsa_key():
