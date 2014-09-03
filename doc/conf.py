0cb14eb3116c3b2e932289f9061b3aa19954719f
Thomas S Hatch <thatch45@gmail.com>
2014-09-03 12:26:36 -0600

    bump version to 1.3.5

--- doc/conf.py
+++ doc/conf.py
@@ -53,7 +53,7 @@ copyright = u'2014, Thomas S Hatch'
 # built documents.
 #
 # The short X.Y version.
-version = '1.3.4'
+version = '1.3.5'
 # The full version, including alpha/beta/rc tags.
 release = version
 
