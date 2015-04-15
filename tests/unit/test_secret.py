ff3a7ad122af4cc1cdfa0b882b2d1d7366d640f2
Peter D. Gray <peter@conalgo.com>
2015-04-15 09:09:52 -0400

    Add failing test for unicode string encryption

--- tests/unit/test_secret.py
+++ tests/unit/test_secret.py
@@ -20,3 +20,23 @@ class TestSecret(unittest.TestCase):
         clear3 = box.decrypt(ctxt2)
         self.assertEqual(clear3, msg)
 
+    def test_unicode_issues(self):
+        msg = u'Unicode string'
+        box = libnacl.secret.SecretBox()
+
+        # Encrypting a unicode string (in py2) should
+        # probable assert, but instead it encryptes zeros,
+        # perhaps the high bytes in UCS-16?
+        ctxt = box.encrypt(msg)
+        self.assertNotEqual(msg, ctxt)
+
+        box2 = libnacl.secret.SecretBox(box.sk)
+        clear1 = box.decrypt(ctxt)
+
+        self.assertEqual(msg, clear1)
+        clear2 = box2.decrypt(ctxt)
+
+        self.assertEqual(clear1, clear2)
+        ctxt2 = box2.encrypt(msg)
+        clear3 = box.decrypt(ctxt2)
+        self.assertEqual(clear3, msg)
