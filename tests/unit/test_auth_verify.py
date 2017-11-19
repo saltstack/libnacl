a6d5d3be18adc795c866cb1e00c1c84c8dea8ed2
Kent Ross <k@mad.cash>
2017-11-19 04:28:11 -0800

    add failing tests

--- tests/unit/test_auth_verify.py
+++ tests/unit/test_auth_verify.py
@@ -57,3 +57,46 @@ class TestAuthVerify(unittest.TestCase):
             libnacl.crypto_onetimeauth_verify(sig2, msg, key1)
         self.assertTrue('Failed to auth message' in context.exception.args)
 
+    def test_auth_rejects_wrong_lengths(self):
+        msg = b'Time is an illusion. Lunchtime doubly so.'
+        for bad_key in (b'too short', b'too long' * 100):
+            with self.assertRaises(ValueError) as context:
+                libnacl.crypto_auth(msg, bad_key)
+            self.assertEqual(context.exception.args, ('Invalid secret key',))
+
+    def test_auth_verify_rejects_wrong_key_lengths(self):
+        msg = b"I'd take the awe of understanding over the awe of ignorance any day."
+        good_key = b'This valid key is 32 bytes long.'
+        good_token = b'This token is likewise also 32B.'
+
+        for bad_key in (b'too short', b'too long' * 100):
+            with self.assertRaises(ValueError) as context:
+                libnacl.crypto_auth_verify(good_token, msg, bad_key)
+            self.assertEqual(context.exception.args, ('Invalid secret key',))
+
+        for bad_token in (b'too short', b'too long' * 100):
+            with self.assertRaises(ValueError) as context:
+                libnacl.crypto_auth_verify(bad_token, msg, good_key)
+            self.assertEqual(context.exception.args, ('Invalid authenticator',))
+
+    def test_onetimeauth_rejects_wrong_lengths(self):
+        msg = b"Are the most dangerous creatures the ones that use doors or the ones that don't?"
+        for bad_key in (b'too short', b'too long' * 100):
+            with self.assertRaises(ValueError) as context:
+                libnacl.crypto_onetimeauth(msg, bad_key)
+            self.assertEqual(context.exception.args, ('Invalid secret key',))
+
+    def test_onetimeauth_verify_rejects_wrong_key_lengths(self):
+        msg = b"Of all the dogs I've known in my life, I've never seen a better driver."
+        good_key = b'This valid key is 32 bytes long.'
+        good_token = b'1time tokens=16B'
+
+        for bad_key in (b'too short', b'too long' * 100):
+            with self.assertRaises(ValueError) as context:
+                libnacl.crypto_onetimeauth_verify(good_token, msg, bad_key)
+            self.assertEqual(context.exception.args, ('Invalid secret key',))
+
+        for bad_token in (b'too short', b'too long' * 100):
+            with self.assertRaises(ValueError) as context:
+                libnacl.crypto_onetimeauth_verify(bad_token, msg, good_key)
+            self.assertEqual(context.exception.args, ('Invalid authenticator',))
