a30c14d2a018ccf58de8314cc9d95852975b12a7
Michael Mendoza <m.mendoza711@gmail.com>
2018-06-13 00:13:19 +0100

    Added bindings for key exchange API. Including crypto_kx_keypair, crypto_kx_seed_keypair, crypto_kx_client_session_keys, crypto_kx_server_session_keys.

--- libnacl/__init__.py
+++ libnacl/__init__.py
@@ -169,6 +169,11 @@ if not DOC_RUN:
     crypto_kdf_BYTES_MAX = nacl.crypto_kdf_bytes_max()
     crypto_kdf_CONTEXTBYTES = nacl.crypto_kdf_contextbytes()
     crypto_kdf_KEYBYTES = nacl.crypto_kdf_keybytes()
+    crypto_kx_PUBLICKEYBYTES = nacl.crypto_kx_publickeybytes()
+    crypto_kx_SECRETKEYBYTES = nacl.crypto_kx_secretkeybytes()
+    crypto_kx_SEEDBYTES = nacl.crypto_kx_seedbytes()
+    crypto_kx_SESSIONKEYBYTES = nacl.crypto_kx_sessionkeybytes()
+    crypto_kx_PRIMITIVE = nacl.crypto_kx_primitive()
 
     # pylint: enable=C0103
 
@@ -1128,6 +1133,50 @@ def crypto_kdf_derive_from_key(subkey_size, subkey_id, context, master_key):
     buf = ctypes.create_string_buffer(size)
     nacl.crypto_kdf_derive_from_key(buf, subkey_size, subkey_id, context, master_key)
     return buf.raw
+
+def crypto_kx_keypair():
+    '''
+    Generate and return a new keypair
+    '''
+    pk = ctypes.create_string_buffer(crypto_kx_PUBLICKEYBYTES)
+    sk = ctypes.create_string_buffer(crypto_kx_SECRETKEYBYTES)
+    nacl.crypto_kx_keypair(pk, sk)
+    return pk.raw, sk.raw
+
+def crypto_kx_seed_keypair(seed):
+    '''
+    Generate and return a keypair from a key seed 
+    '''
+    if len(seed) != crypto_kx_SEEDBYTES:
+        raise ValueError('Invalid key seed')
+    pk = ctypes.create_string_buffer(crypto_kx_PUBLICKEYBYTES)
+    sk = ctypes.create_string_buffer(crypto_kx_SECRETKEYBYTES)
+    nacl.crypto_kx_seed_keypair(pk, sk, seed)
+    return pk.raw, sk.raw
+
+def crypto_kx_client_session_keys(rx, tx, client_pk, client_sk, server_pk):
+    '''
+    Computes a pair of shared keys (rx and tx) using the client's public key client_pk, 
+    the client's secret key client_sk and the server's public key server_pk.
+    Status returns 0 on success, or -1 if the server's public key is not acceptable.
+    '''
+    rx = ctypes.create_string_buffer(crypto_kx_SESSIONKEYBYTES)
+    tx = ctypes.create_string_buffer(crypto_kx_SESSIONKEYBYTES)
+    status = nacl.crypto_kx_client_session_keys(rx, tx, client_pk, client_sk, server_pk)
+    return rx, tx, status
+
+def crypto_kx_server_session_keys(rx, tx, server_pk, server_sk, client_pk):
+    '''
+    Computes a pair of shared keys (rx and tx) using the server's public key server_pk, 
+    the server's secret key server_sk and the client's public key client_pk.
+    Status returns 0 on success, or -1 if the client's public key is not acceptable.
+    '''
+    rx = ctypes.create_string_buffer(crypto_kx_SESSIONKEYBYTES)
+    tx = ctypes.create_string_buffer(crypto_kx_SESSIONKEYBYTES)
+    status = nacl.crypto_kx_server_session_keys(rx, tx, server_pk, server_sk, client_pk)
+    return rx, tx, status
+
+
 
 # Utility functions
 
